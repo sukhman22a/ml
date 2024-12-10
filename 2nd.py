3e18001574f100dabd55ce365c0c3ad91dbd882d
sukhman <sukhmansinghsardar@gmail.com>
2024-12-10 20:27:09 +0530

    what is that

--- 2nd.py
+++ 2nd.py
@@ -6,10 +6,12 @@ df = pd.read_csv("data\homeprice.csv")
 reg = linear_model.LinearRegression() 
 
 reg.fit(df[["area"]].values,df.price)
-# .values returns only the data from the DataFrame, not the column names.
-# If you don't use it properly, it may cause a warning."
-print(reg.predict([[3030]]))
 
-
+test_df = pd.read_csv("data\homeprice_test.csv")
+test_df["prices"] = reg.predict(test_df[["area"]].values)
+coeff = reg.coef_
+incpt = reg.intercept_
+test_df["checkPrice"] = coeff*test_df[["area"]] + incpt
+print(test_df)
 print("hello")
 
