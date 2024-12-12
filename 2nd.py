be9ea2a4cb12439b38981eeb6cad94a76df8c102
sukhman <sukhmansinghsardar@gmail.com>
2024-12-12 11:34:36 +0530

    plot problem

--- 2nd.py
+++ 2nd.py
@@ -1,17 +1,35 @@
+# plot has some problem
+''''it plots lines correctly 
+but there is some problem with points(marker) 
+there is a gap between years where there are no points
+'''
 import pandas as pd
 import matplotlib.pyplot as mpl
 from sklearn import linear_model
 import numpy as np
-df = pd.read_csv("data\homeprice.csv")
+df = pd.read_csv("data/canada_per_capita_income.csv")
 reg = linear_model.LinearRegression() 
 
-reg.fit(df[["area"]].values,df.price)
-
-test_df = pd.read_csv("data\homeprice_test.csv")
-test_df["prices"] = reg.predict(test_df[["area"]].values)
+reg.fit(df[["year"]].values,df['per capita income (US$)'].values)
 coeff = reg.coef_
 incpt = reg.intercept_
-test_df["checkPrice"] = coeff*test_df[["area"]] + incpt
-print(test_df)
-print("hello")
 
+
+analysed_df = pd.read_csv("analysed/2nd.csv")
+last_year = analysed_df['year'].max()
+till_now = int(input(f"enter the year above {last_year}: "))
+
+new_prediction = []
+for i in range(last_year,till_now+1):
+    predicted_revenue = reg.predict([[i]])
+    new_record = {"year" : i,"per capita income (US$)" : predicted_revenue}
+    new_prediction.append(new_record)
+
+new = pd.DataFrame(new_prediction)
+df = pd.concat([df,new] , ignore_index=True) 
+mpl.plot(df[['year']],df[["per capita income (US$)"]],marker='.')
+mpl.xlabel("Year")
+mpl.ylabel("Per Capita Income (US$) ")
+mpl.show()
+file_path = "C:/Users/LENOVO/Desktop/ml/ml/analysed/2nd.csv"
+df.to_csv(file_path,index=False,na_rep="NaN")
