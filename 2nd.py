2e01a794c29aaa2c2acec8d651b8635247b5bc5b
sukhman <sukhmansinghsardar@gmail.com>
2024-12-23 03:57:44 +0530

    UNcomplelete gradient descent

--- 2nd.py
+++ 2nd.py
@@ -1,27 +1,32 @@
-""" Hiring Analysis(exercise) """
+"""gradient descent 
+https://youtu.be/sDv4f4s2SB8?feature=shared
+https://youtu.be/vsWrXfO3wWw?feature=shared    15:21sec"""
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as mpl
-import word2number.w2n as w2n
-from sklearn import linear_model
-# import numpy as np
-df = pd.read_csv("data/hiring.csv")
-reg = linear_model.LinearRegression() 
+# from sklearn import linear_model
+def gradient_descent(x,y):
+    m_curr = b_curr = 0
+    n= len(x)
+    iterations = 1000
+    learning_rate = 0.0002
+    for i in range(iterations):
+        # 
+        y_predicated = m_curr*x + b_curr
+        cost = (1/n)*sum([val **2 for val in (y-y_predicated)])
+        md = -(2/n)*sum((y - y_predicated)*x)
+        bd = -(2/n)*sum(y - y_predicated)
+        m_curr -= md*learning_rate
+        b_curr -= bd*learning_rate
+        print("m {}, b {} cost {} iteration {}".format(m_curr,b_curr,cost,i))
+    return b_curr, m_curr     
 
+df = pd.read_csv("data/marks.csv")
+# print(df)
+# x = np.array([1,2,3,4,5])
+# y = np.array([5,7,9,11,13])
 
-# The 'experience' column may contain missing values (NaN). 
-# We first replace NaN values with the string "zero" using fillna(). 
-# Then, we apply the word_to_num function (from the w2n library) to convert 
-# the words (e.g., "one", "two", "three") into corresponding numerical values. 
-# If 'experience' is NaN, it will be treated as "zero" and converted to 0.
-df["experience_in_number"] = df["experience"].fillna("zero").apply(w2n.word_to_num)
-df["test_score(out of 10)"] = df["test_score(out of 10)"].fillna(df["test_score(out of 10)"].median())
-
-
-reg.fit(df[["test_score(out of 10)","interview_score(out of 10)","experience_in_number"]].values,df[["salary($)"]].values)
-
-# print(reg.predict([[9,6,2]]))
-# "test_score(out of 10)","interview_score(out of 10)","experience_in_number"
-mpl.plot(df["experience_in_number"].values,df["salary($)"].values,"bo")
-mpl.xlabel("Experience")
-mpl.ylabel("Salary($)")
-mpl.show()
+bg ,mg = gradient_descent(df["math"],df["cs"])
+# reg = linear_model.LinearRegression()
+# reg.fit(df[["math"]].values,df[["cs"]])
+print("bg {}    mg {}\n".format(bg,mg))
+# print("b {}  m {}".format(reg.intercept_,reg.coef_))  #b [1.91521931]  m [[1.01773624]]
