4c4dfc6eea3c920a17886c609a769b8fac0d112a
sukhman <sukhmansinghsardar@gmail.com>
2024-12-23 10:33:55 +0530

    dummy variables (sklearn OneHotEncoder) didn't understand

--- 2nd.py
+++ 2nd.py
@@ -4,22 +4,10 @@
 
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
 
 df = pd.read_csv("data/carprices.csv")
-dummies = pd.get_dummies(df["Car Model"]).astype(int)
-df_updated = pd.concat([df,dummies],axis="columns")
-final = df_updated.drop(["Car Model","Audi A5"], axis="columns")
+ohe = OneHotEncoder()
 
-x = final.drop(["Sell Price($)"],axis="columns")
-y = final["Sell Price($)"]
-
-model = LinearRegression()
-model.fit(x.values,y.values)
-
-pre = model.predict([[70000,3,0,0]])
-# mileage , age(yrs), BMW X5(0),mercedez benz c class(0),audi a5(1)
-
-print(model.predict([[45000,4,0,1]]))
-print(model.predict([[86000,7,1,0]]))
-print(model.score(x.values,y.values))
-# x.values give the values in numoy array and x alone give dataframe
+dfle = df
+dfle["Car Model"] = ohe.fit_transform(dfle[["Car Model"]]).values
