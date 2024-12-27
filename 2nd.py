1b1fe58792b1ad833f791fd4d6fbc4c71c94910c
sukhman <sukhmansinghsardar@gmail.com>
2024-12-28 01:13:30 +0530

    support Vector Machine ?????

--- 2nd.py
+++ 2nd.py
@@ -1,18 +1,20 @@
-from sklearn.model_selection import train_test_split
-from sklearn import tree 
-import pandas as pd
-df = pd.read_csv("data/titanic.csv")
-# print(df)
-required_df = df[["Survived","Pclass","Sex","Age","Fare"]]
-x = required_df.drop(["Survived"],axis="columns")
-y = required_df["Survived"]
-dummies = pd.get_dummies(required_df["Sex"]).astype(int)
-x = pd.concat([x,dummies],axis="columns")
-x = x.drop(["Sex","male"],axis="columns")
-x["Age"] = x['Age'].fillna(x["Age"].median())
-x["Fare"] = x['Fare'].fillna(x["Fare"].mean()) 
+'''Support Vector Machine 
+for dividing two datasets
+https://youtu.be/FB5EdxAGxQg?feature=shared'''
 
-X_train, X_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
-model = tree.DecisionTreeClassifier()
-model.fit(X_train,y_train)
-print(model.score(X_test,y_test))
+
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt 
+import pandas as pd 
+from sklearn.svm import SVC
+
+digits= load_digits()
+# 'DESCR', 'data', 'feature_names', 'frame', 'images', 'target', 'target_names'
+df  = pd.DataFrame(digits.data, columns=digits.feature_names)
+y = digits.target
+x = df
+model = SVC(C=6,kernel="linear",gamma="scale")
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2 ,random_state=1)
+model.fit(x_train,y_train)
+print(model.score(x_test,y_test))
