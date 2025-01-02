ab7010e475ed8863f73fe3f0879ff0258506e37e
sukhman <sukhmansinghsardar@gmail.com>
2025-01-02 20:26:10 +0530

    Pipe lining(very minimal use )

--- 2nd.py
+++ 2nd.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import cross_val_score
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
 
 df = pd.read_csv("data/spam.csv")
 # print(df.groupby("Category").describe())
@@ -15,9 +16,10 @@ df["spam"] = df["Category"].apply(lambda x : 0 if 'ham' == x else 1)
 df = df.drop(["Category"],axis="columns")
 x_train ,x_test ,y_train ,y_test = train_test_split(df["Message"],df["spam"],test_size=0.3)
 
-coder = CountVectorizer()
-x = coder.fit_transform(df["Message"])
-print(x.toarray().shape)
-
-score = cross_val_score(MultinomialNB(),x,df["spam"])
-print(score.mean())
+clf = Pipeline([
+    ("verctorizer",CountVectorizer()),
+    ("mnb",MultinomialNB()),
+    ("score", cross_val_score())
+])
+clf.fit(x_train,y_train)
+print(clf.score(x_test,y_test))
